c8c192d8dc0971d9bad4a4acf26caca38b4ccd71
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-17 22:21:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def find_min_in_nested_arrays(src)
 new_array = [] 
 row_index = 0 
-min_value = -1
+min_value = 500
  while row_index < src.count do 
    element_index = 0 
    while element_index < src[row_index].count do  
-     if min_value < src[row_index][element_index]
+     if min_value > src[row_index][element_index]
        min_value = src[row_index][element_index]
      end
      row_index += 1 
