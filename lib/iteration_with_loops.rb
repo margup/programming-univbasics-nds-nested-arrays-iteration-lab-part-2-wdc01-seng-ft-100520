9611a2e47ce1bdfcd85aac81ffa71fb63276aed7
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-17 22:27:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,10 +3,10 @@ def find_min_in_nested_arrays(src)
   row_index=0 
   new_array = []
   
-  
   while row_index < src.count do 
     element_index = 0 
     
+    min = src[row_index][element_index]
     while element_index < src[row_index].count do
       
       if src[row_index][element_index] < min
@@ -18,6 +18,7 @@ def find_min_in_nested_arrays(src)
    row_index += 1 
   end
    return new_array
+
      
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
